e7a2e3427eedf23e984af959ccd97c2227b64e1d
cburrows87 <chris.burrows@listers.co.uk>
2020-02-26 10:09:47 +0000

    Added React to Solution

---
+++ client-app/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+ 
+    </div>
+  );
+}
+
+export default App;
